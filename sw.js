9e1d116a344f132b2bddc70683bfc69365e3e26a
dmarchese224 <doug.marchese@gmail.com>
2026-05-09 15:46:08 -0400

    delete to start the perplexity

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Basic service worker to satisfy PWA requirements
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  // Pass-through for network requests
-});
